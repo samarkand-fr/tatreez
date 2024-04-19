fb22ac8a9260d7e7953cbc04a06b1ea4a30c4a9e
dina <jdina2004@gmail.com>
2024-04-19 10:43:00 +0200

    fix menu functionality  on mobile,improve css

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,3 @@
-// Getting hamburguer menu in small screens
-const menu = document.getElementById("menu");
-const ulMenu = document.getElementById("ulMenu");
-
 function menuToggle() {
-  menu.classList.toggle("h-32");
-}
-
-// Browser resize listener
-window.addEventListener("resize", menuResize);
-
-// Resize menu if user changing the width with responsive menu opened
-function menuResize() {
-  // First get the size from the window
-  const window_size = window.innerWidth || document.body.clientWidth;
-  if (window_size > 640) {
-    menu.classList.remove("h-32");
-  }
-  function menuResize() {
-    const menu = document.getElementById('menu');
-    if (menu) {
-        if (window.innerWidth > 768) {
-            menu.classList.remove('hidden');
-        } else {
-            menu.classList.add('hidden');
-        }
-    }
-}
-
-window.addEventListener('resize', menuResize);
-
+  document.getElementById("nav").classList.toggle("hidden");
 }
